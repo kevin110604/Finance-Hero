c17f3b074d333d54757c0b67d904791cf3e897cc
ahao <w11179627@gmail.com>
2018-06-16 12:58:36 +0800

    shop panel visable mistake2

--- FinanceHero/Form1.cs
+++ FinanceHero/Form1.cs
@@ -82,8 +82,8 @@ namespace FinanceHero
 
         private void Shopbutton_Click(object sender, EventArgs e)
         {
-            Homepanel.Visible = true;
-            Shoppanel.Visible = false;
+            Homepanel.Visible = false;
+            Shoppanel.Visible = true;
             Heropanel.Visible = false;
             Statispanel.Visible = false;
         }
